d3fe8bc436b03e093e56d2133f6eae1bc42eff4b
TezRomacH <tez.romach@gmail.com>
2017-12-25 10:02:21 +0300

    train neural net on MNIST

---
+++ mnist_example.py
@@ -0,0 +1,40 @@
+from tezromach.layers import Normalization, Linear, Tanh, Sigmoid
+from tezromach.network import NeuralNet
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import preprocessing
+
+encoder = preprocessing.OneHotEncoder(sparse=False)
+
+inputs = np.genfromtxt("mnist_data/inputs.csv", delimiter=',')
+targets = np.genfromtxt("mnist_data/targets.csv", delimiter=',')
+targets = encoder.fit_transform(np.array(targets).reshape(-1, 1))
+
+inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs, targets, test_size=0.15)
+print("inputs shape =", inputs.shape)
+print("targets shape =", targets.shape)
+
+net = NeuralNet([
+    Normalization(),
+
+    Linear(input_size=64, output_size=100),
+    Sigmoid(),
+    Linear(input_size=100, output_size=80),
+    Sigmoid(),
+    Linear(input_size=80, output_size=10),
+    Sigmoid()
+])
+
+print("start fit NeuralNet")
+net.fit(inputs_train, targets_train, verbose_print=10, num_epochs=200, learning_rate=0.1)
+print("done fit NeuralNet")
+print(net)
+
+net.save_model("model_mnist")
+predicted = net.predict(inputs_test)
+
+predicted = np.argmax(predicted, axis=1)
+targets_test = np.argmax(targets_test, axis=1)
+
+print("Accuracy: ", metrics.accuracy_score(predicted, targets_test))
--- xor.py
+++ xor.py
@@ -5,7 +5,7 @@ learned with a simple linear model is XOR
 import numpy as np
 
 from tezromach.network import NeuralNet
-from tezromach.layers import Linear, Sigmoid, Tanh, Normalization
+from tezromach.layers import Linear, Tanh, Normalization
 
 inputs = np.array([
     [0, 0],
@@ -24,12 +24,12 @@ targets = np.array([
 net = NeuralNet([
     Normalization(),
     Linear(input_size=2, output_size=2),
-    Sigmoid(),
+    Tanh(),
     Linear(input_size=2, output_size=1),
     Tanh()
 ])
 
-net.fit(inputs, targets, num_epochs=15000, learning_rate=0.02)
+net.fit(inputs, targets, num_epochs=15000, learning_rate=0.03)
 
 for x, y in zip(inputs, targets):
     predicted = net.predict(x)
